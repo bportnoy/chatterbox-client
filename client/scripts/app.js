6ea64e97257ce9a5ef1425e0ff86c2cf8d38c4ad
bportnoy <bradley.portnoy@gmail.com>
2015-02-16 17:00:15 -0800

    Sending and receiving

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,80 @@
 // YOUR CODE HERE:
 
+//get message function:
+//get message from Parse
+//escape message content
+//display and append to body
+
+var getMessages = function(number){
+  var options = {order:'-createdAt',limit:number};
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    //data: JSON.stringify(message),
+    contentType: 'application/json',
+    data: options,
+    success: function (data) {
+      displayMessages(data);
+      console.log(data);
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to receive message');
+    }
+  });
+};
+
+var displayMessages = function(data){
+  var $stream = $('#stream');
+  var messages = data.results;
+  _.each(messages,function(value){
+    if (!value.username) value.username = "Anaughtymouse";
+    $stream.prepend('<li>' + validator.escape(value.username) + ': "' +validator.escape(value.text)+'"</li>');
+  });
+  setTimeout(function(){getMessages(1)},1000);
+};
+
+var displayOurMessage = function(text){
+  var $stream = $('#stream');
+  var text = validator.escape(text);
+  $stream.append('<li>'+document.createTextNode(text).nodeValue+'</li>');
+};
+
+var postMessage = function(name,message,room){
+  var data = {
+    username: name,
+    text: message,
+    roomname: room
+  };
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(data),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+$(document).ready(function(){
+  $send = $('#sendButton');
+  $box = $('#writeMessageBox');
+  $send.on('click',function(){
+    var input = $box.val();
+    // console.log('test');
+    postMessage("testname",input,"lobby");
+    $box.val('');
+  });
+});
+
+getMessages(1);
+
+
+//function: escape message content
